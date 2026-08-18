02d2ca12ef080d488582f4d0e832b5d4f7a94319
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-08-18 20:19:23 +0200

    feat/test(free): add cleaning function

---
+++ src/free.c
@@ -0,0 +1,30 @@
+#include "../include/codexion.h"
+
+void    cleanup(t_data *data)
+{
+    int     i;
+
+    i = -1;
+    while (++i < data->number_of_coders)
+    {
+        pthread_mutex_destroy(&data->coders[i].mutex);
+        pthread_cond_destroy(&data->coders[i].cond);
+        pthread_mutex_destroy(&data->dongles[i].mutex);
+        pthread_cond_destroy(&data->dongles[i].cond);
+    }
+    if (data->coders)
+        free(data->coders);
+    if (data->heap->node)
+        free(data->heap->node);
+    if (data->heap)
+        free(data->heap);
+    pthread_mutex_destroy(&data->ready_mutex);
+    pthread_mutex_destroy(&data->end_mutex);
+    pthread_mutex_destroy(&data->write_mutex);
+    pthread_cond_destroy(&data->monitor_cond);
+    pthread_cond_destroy(&data->ready_cond);
+
+
+
+    i = -1;
+}
