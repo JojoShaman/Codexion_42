13f1fb1c869bb28c1e1abc33c87a5fee87dda9d7
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-08-19 21:48:09 +0200

    fix(norme): norme fixed

--- src/heap/heap_utils.c
+++ src/heap/heap_utils.c
@@ -1,51 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   heap_utils.c                                      :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: srosu <srosu@student.42belgium.be>        #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/08/19 19:26:07 by srosu            #+#    #+#              */
+/*   Updated: 2026/08/19 19:26:07 by srosu           ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
 #include "../../include/codexion.h"
 
-void    heapify(t_heap *arr, int i)
+void	heapify(t_heap *arr, int i)
 {
-    t_heap_node     tmp;
-    int     parent;
-    int     child1;
-    int     child2;
-    int     size;
+	t_heap_node	tmp;
+	int			parent;
+	int			child1;
+	int			child2;
+	int			size;
 
-    tmp = arr->node[i];
-    parent = i;
-    child1 = 2 * i + 1;
-    child2 = 2 * i + 2;
-    size = arr->size;
-
-    if (child1 < size &&
-        arr->node[child1].deadline < arr->node[parent].deadline)
-        parent = child1;
-    if (child2 < size &&
-        arr->node[child2].deadline < arr->node[parent].deadline)
-        parent = child2;
-    if (parent != i)
-    {
-        tmp = arr->node[i];
-        arr->node[i] = arr->node[parent];
-        arr->node[parent] = tmp;
-        arr->node[parent].coder->heap_index = parent;
-        arr->node[i].coder->heap_index = i;
-        heapify(arr, parent);
-    }
-}
-
-void    sift_up(t_heap *heap, int i)
-{
-    int             parent;
-    t_heap_node     tmp;
-
-    while (i > 0)
-    {
-        parent = (i - 1) / 2;
-        if (heap->node[parent].deadline <= heap->node[i].deadline)
-            break ;
-        tmp = heap->node[i];
-        heap->node[i] = heap->node[parent];
-        heap->node[parent] = tmp;
-        heap->node[i].coder->heap_index = i;
-        heap->node[parent].coder->heap_index = parent;
-        i = parent;
-    }
+	parent = i;
+	child1 = 2 * i + 1;
+	child2 = 2 * i + 2;
+	size = arr->size;
+	if (child1 < size
+		&& arr->node[child1].deadline < arr->node[parent].deadline)
+		parent = child1;
+	if (child2 < size
+		&& arr->node[child2].deadline < arr->node[parent].deadline)
+		parent = child2;
+	if (parent != i)
+	{
+		tmp = arr->node[i];
+		arr->node[i] = arr->node[parent];
+		arr->node[parent] = tmp;
+		arr->node[parent].coder->heap_index = parent;
+		arr->node[i].coder->heap_index = i;
+		heapify(arr, parent);
+	}
 }
