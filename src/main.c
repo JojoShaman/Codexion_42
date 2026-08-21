a6862cb9f84727f16642b1576f04cacc0a215d2c
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-08-21 02:18:55 +0200

    fix(main): cleanup if simulation failed instead of just free(data)

--- src/main.c
+++ src/main.c
@@ -9,27 +9,28 @@
 /*   Updated: 2026/08/19 19:26:07 by srosu           ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include "../include/codexion.h"
 
-int     main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    t_data  *data;
+	t_data	*data;
 
-    data = malloc(sizeof(*data));
-    if (!data)
-        return (1);
-    if (!data_validator(argc, argv))
-    {
-        free(data);
-        return (1);
-    }
-    init_data(data, argv);
-    if (!init_simulation(data))
-    {
-        fprintf(stderr, "MALLOC FAILED");
-        free(data);
-        return (1);
-    }
-    cleanup(data);
-    return (0);
+	data = malloc(sizeof(*data));
+	if (!data)
+		return (1);
+	if (!data_validator(argc, argv))
+	{
+		free(data);
+		return (1);
+	}
+	init_data(data, argv);
+	if (!init_simulation(data))
+	{
+		fprintf(stderr, "MEMORY ALLOCATION FAILED");
+		cleanup(data);
+		return (1);
+	}
+	cleanup(data);
+	return (0);
 }
