c926dbd2589b6c596d429336fcc8574983c13697
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-08-21 02:10:52 +0200

    refactor(scheduling_utils): extract dongle_release & add helpers (is_waiting, winner, safe_if_end, priority)

---
+++ src/threads/scheduling_utils.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   scheduling_utils.c                                :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: srosu <srosu@student.42belgium.be>        #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/08/19 19:26:07 by srosu            #+#    #+#              */
+/*   Updated: 2026/08/19 19:26:07 by srosu           ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/codexion.h"
+
+void	dongle_release(t_dongle *dongle)
+{
+	pthread_mutex_lock(&dongle->mutex);
+	dongle->taken = false;
+	dongle->last_release = get_time(MILLISECOND);
+	pthread_cond_broadcast(&dongle->cond);
+	pthread_mutex_unlock(&dongle->mutex);
+}
+
+bool	is_waiting(t_mtx *mutex, t_dongle *requested, t_dongle *current)
+{
+	bool	ret;
+
+	ret = false;
+	pthread_mutex_lock(mutex);
+	if (requested == current)
+		ret = true;
+	pthread_mutex_unlock(mutex);
+	return (ret);
+}
+
+t_coder	*winner(long long right, long long left, t_dongle *dongle)
+{
+	if (right < left)
+		return (dongle->right);
+	else
+		return (dongle->left);
+	return (NULL);
+}
+
+bool	safe_if_end(t_coder *coder, t_dongle *dongle)
+{
+	pthread_mutex_lock(&coder->mutex);
+	coder->waiting_for = NULL;
+	pthread_mutex_unlock(&coder->mutex);
+	pthread_mutex_unlock(&dongle->mutex);
+	return (false);
+}
+
+t_coder	*priority(t_coder *coder, t_dongle *dongle)
+{
+	if (coder->data->scheduler == FIFO)
+		return (first_arrived(dongle));
+	else
+		return (shortest_deadline(dongle));
+	return (NULL);
+}
