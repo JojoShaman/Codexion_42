28f0d588088bd13b088234741e61f1436bd04b4e
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-08-21 02:21:46 +0200

    fix(monitor): guard against null pointer dereference when heap size reaches 0

--- src/threads/monitor.c
+++ src/threads/monitor.c
@@ -1,27 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   monitor.c                                         :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: srosu <srosu@student.42belgium.be>        #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/08/19 19:26:07 by srosu            #+#    #+#              */
+/*   Updated: 2026/08/19 19:26:07 by srosu           ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../../include/codexion.h"
+
+static void	burned_out(t_data *data, t_heap *heap)
+{
+	set_end(data);
+	output(heap->node[0].coder, BURNED_OUT);
+}
 
 void	*monitoring(void *all_data)
 {
-	t_heap          *heap;
-	t_data          *data;
-	struct timespec ts;
-	int             rt;
+	t_data			*data;
+	struct timespec	ts;
+	int				rt;
 
-	heap = NULL;
 	data = (t_data *)all_data;
-	heap = data->heap;
-	while(!is_end(data))
+	while (!is_end(data))
 	{
 		pthread_mutex_lock(&data->heap->mutex);
-		ts.tv_sec = heap->node[0].deadline / 1000;
-		ts.tv_nsec = (heap->node[0].deadline % 1000) * 1000000;
-		rt = pthread_cond_timedwait(&data->monitor_cond, &data->heap->mutex, &ts);
-		if (rt == ETIMEDOUT && !is_end(data)
-			&& !reached_compile_target(heap->node[0].coder))
+		if (data->heap->size == 0)
 		{
-			set_end(data);
-			output(heap->node[0].coder, BURNED_OUT);
+			pthread_mutex_unlock(&data->heap->mutex);
+			break ;
 		}
+		ts.tv_sec = data->heap->node[0].deadline / 1000;
+		ts.tv_nsec = (data->heap->node[0].deadline % 1000) * 1000000;
+		rt = pthread_cond_timedwait(&data->monitor_cond,
+				&data->heap->mutex, &ts);
+		if (rt == ETIMEDOUT && !is_end(data)
+			&& !reached_compile_target(data->heap->node[0].coder))
+			burned_out(data, data->heap);
 		pthread_mutex_unlock(&data->heap->mutex);
 	}
 	return (NULL);
